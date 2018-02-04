6eb33204f449f96525c546a2badbb11075f28c20
dkhamitov <khamitov.dm@gmail.com>
2018-02-04 15:07:36 +0300

    Keep the photo names as their server's paths

--- src/main/kotlin/d/kh/VkPhotoLoader.kt
+++ src/main/kotlin/d/kh/VkPhotoLoader.kt
@@ -7,6 +7,7 @@ import com.vk.api.sdk.objects.photos.PhotoAlbumFull
 import org.apache.http.client.methods.HttpGet
 import org.apache.http.impl.client.HttpClients
 import java.io.File
+import java.net.URI
 import java.nio.file.Paths
 
 /**
@@ -35,8 +36,10 @@ fun main(args: Array<String>) {
 
     printAlbums(albums)
 
-    val albumToLoad = readString("Which album to download?")
+    println()
+    val albumToLoad = readString("Which album to download?", { albumTitleById.contains(it) })
     val albumFolder = readString("Where to download the album?")
+
     val photos = vk.photos()
             .get(actor)
             .ownerId(userId)
@@ -61,13 +64,23 @@ private fun printAlbums(albums: Collection<PhotoAlbumFull>) {
 }
 
 private fun savePhotos(urls: List<String>, root: String, album: String) {
+    val urlsByCopies = urls.groupBy { it }
+            .map { it.key to it.value.size }
+            .sortedByDescending { it.second }
+    val duplicates = urlsByCopies.takeWhile { it.second > 1 }
+    if (duplicates.isNotEmpty()) {
+        println("There are duplicates:")
+        duplicates.forEach { println("${it.first} ${it.second} copies") }
+    }
+    println("${urlsByCopies.size} photos out of ${urls.size} will be downloaded")
+
     val folder = Paths.get(root, album).toFile()
     folder.mkdirs()
+
     val httpClient = HttpClients.createDefault()
-    urls.forEachIndexed { i, url ->
-        val prefix = i + 1
-        val file = File(folder, "$prefix-${url.substringAfterLast("/")}")
+    urlsByCopies.map { it.first }.forEachIndexed { i, url ->
+        val file = File(folder, URI.create(url).path.substring(1).replace('/', '-'))
         httpClient.execute(HttpGet(url)).entity.writeTo(file.outputStream())
-        println("$prefix downloaded")
+        println("$url downloaded (${i + 1} of ${urls.size})")
     }
 }
--- src/main/kotlin/d/kh/VkUtil.kt
+++ src/main/kotlin/d/kh/VkUtil.kt
@@ -7,6 +7,14 @@ fun readString(prompt: String): String {
     return Scanner(System.`in`).next()
 }
 
+fun readString(prompt: String, successCond: (String) -> Boolean): String {
+    var value = readString(prompt)
+    while (!successCond(value)) {
+        value = readString("Wrong input! $prompt")
+    }
+    return value
+}
+
 fun readInt(prompt: String): Int {
     println(prompt)
     return Scanner(System.`in`).nextInt()
